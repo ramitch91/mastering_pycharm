c089f8f1d2e2aff5a498e71ba113d10383064350
Ricky Mitchell <ramitch91@gmail.com>
2022-04-10 08:22:23 -0500

    Original commit

---
+++ demos/projects/podcast/program.py
@@ -0,0 +1,17 @@
+import service
+
+
+def main():
+    print('Welcome to the Talk Python info downloader')
+    print()
+
+    service.download_info()
+
+    for show_id in range(100, 130):
+        info = service.get_episode(show_id)
+
+        print(f'{info.show_id}. {info.title}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ demos/projects/podcast/service.py
@@ -0,0 +1,32 @@
+import typing
+from collections import namedtuple
+from xml.etree import ElementTree
+
+import requests as requests
+
+Episode = namedtuple('Episode', 'title link pubDate show_id')
+episode_data = {}
+
+
+def download_info():
+    url = 'https://talkpython.fm/episodes/rss'
+
+    resp = requests.get(url)
+    resp.raise_for_status()
+
+    dom = ElementTree.fromstring(resp.text)
+    items = dom.findall('channel/item')
+    episode_count = len(items)
+
+    for idx, item in enumerate(items):
+        episode = Episode(
+            item.find('title').text,
+            item.find('link').text,
+            item.find('pubDate').text,
+            episode_count - idx - 1
+        )
+        episode_data[episode.show_id] = episode
+
+
+def get_episode(show_id: int) -> typing.Optional[Episode]:
+    return episode_data.get(show_id)
