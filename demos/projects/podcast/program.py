a18a651ca3dfb328a582199bf1259d24aca2316c
Ricky Mitchell <ramitch91@gmail.com>
2022-04-10 09:13:33 -0500

    Refactored into some methods

--- demos/projects/podcast/program.py
+++ demos/projects/podcast/program.py
@@ -2,15 +2,21 @@ import service
 
 
 def main():
-    print('Welcome to the Talk Python info downloader')
-    print()
-
+    show_header()
     service.download_info()
+    display_results()
 
+
+def display_results():
     for show_id in range(120, 150):
         info = service.get_episode(show_id)
 
         print(f'{info.show_id}. {info.title}')
+
+
+def show_header():
+    print('Welcome to the Talk Python info downloader')
+    print()
 
 
 if __name__ == '__main__':
